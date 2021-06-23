827c7acfcb9582ee9e965744f13bb9f5916d9fed
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-06-23 13:59:10 +0100

    added basic accordion to contain the questions for each category

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.scss';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import QuestionPage from './components/QuestionPage';
 import HomeHero from './components/HomeHero';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
+
 
 function App() {
   const questions = require("./questions.json")
@@ -15,21 +26,30 @@ function App() {
         <Router>
 
           <Route exact path={process.env.PUBLIC_URL + "/"}>
+            {/* Hero Row of the Homepage */}
             <HomeHero />
 
-            {
-              Object.entries(questions).map(([k, v]) => (
-                <div>
-                  <h2 className="question-type">{k}</h2>
-                  {
-                    v.map((x, index) =>
-                    <Link key={index} to={process.env.PUBLIC_URL + "/"+k+"/"+index}>
-                      <p className="question-link">({index}) {x}</p>
-                    </Link>)
-                  }
-                </div>
-              ))
-            }
+            {/* The Actual Question and Links */}
+            <Accordion allowMultipleExpanded allowZeroExpanded >
+              {
+                Object.entries(questions).map(([k, v]) => (
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>{k}</AccordionItemButton>
+                    </AccordionItemHeading>
+                    {
+                      v.map((x, index) =>
+                      <AccordionItemPanel>
+                        <Link key={index} to={process.env.PUBLIC_URL + "/"+k+"/"+index}>
+                          <p className="question-link">({index}) {x}</p>
+                        </Link>
+                      </AccordionItemPanel>
+                      )
+                    }
+                  </AccordionItem>
+                ))
+              }
+            </Accordion>
           </Route>
 
           {
