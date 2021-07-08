1b8ec0553fde116827346c77a0e64bf395d4c7fd
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-07-08 12:56:06 +0100

    checkpoint before removing all styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import LandingPageHero from './LandingPageComponents/LandingPageHero';
-import LandingPageExplanation from './LandingPageComponents/LandingPageExplanation'
 function LandingPage() {
     return (
         <React.Fragment>
             <LandingPageHero/>
-            {/* <LandingPageExplanation/> */}
         </React.Fragment>
     )
 }
