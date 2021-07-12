93736dcb8249aaea9a89d2f9ddb011b4b95aabee
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-07-12 15:16:32 +0100

    Removed the LandingPage, Router and Nav - the app is now just the QuestionPage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Nav from './components/Nav'
-import LandingPage from './components/LandingPage';
 import QuestionPage from './components/QuestionPage';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Router >
-        {/* <Nav /> */}
         <div className="app-content">
-
-          <Route exact path={process.env.PUBLIC_URL + "/"}>
             <QuestionPage />
-          </Route>
-
-          <Route exact path={process.env.PUBLIC_URL + "/question"}>
-            <QuestionPage />
-          </Route>
-
         </div>
         <Footer />
-      </Router>
     </div>
   )
 }
